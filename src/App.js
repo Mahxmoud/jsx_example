a3a58a27070b8493444b3cea496acedce131d7d5
mahxmoud <mahmoud.jomaa@yahoo.com>
2020-12-04 12:26:37 +0200

    first commin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import './style.css';
+import myImg from './imageInSrc.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ border: "solid 1px black",maxWidth:"100vw" }}>
+          <h1 className="title red">Mahmoud Joma</h1>
+        <br/>
+          <img src={myImg} alt="me, with cool background" />
+        <br/>
+        <img src="/imageInPublic.jpg" alt="me, Rick and Morty having fun"/>
+      </div>
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
       </header>
-    </div>
+    </div >
   );
 }
 
